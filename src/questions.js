9d3217cc5eca44d0ccc40206206d2c5fa9b2d96e
pemiinem <190139323@aluno.unb.br>
2024-10-19 16:19:14 -0300

    refactor: update questions and improve styling

--- src/questions.js
+++ src/questions.js
@@ -1,32 +1,53 @@
 export const questions = {
-    "Category 1": [
-      { question: "Question 1-1", answer: "Answer 1-1" },
-      { question: "Question 1-2", answer: "Answer 1-2" },
-      { question: "Question 1-3", answer: "Answer 1-3" },
+    "Novo Testamento": [
+      { question: "Onde Paulo morreu?",
+        answer: "Roma",
+        image: "../images/roma.png" },
+      { question: "Qual o nome da mãe de Arão, Moisés e Miriã?",
+        answer: "Joquebede",
+        image: "../images/nt3.jpg"
+      },
+      { question: "Qual rei Jesus comparou a uma raposa?", answer: "Herodes" },
     ],
-    "Category 2": [
-      { question: "Question 2-1", answer: "Answer 2-1" },
-      { question: "Question 2-2", answer: "Answer 2-2" },
-      { question: "Question 2-3", answer: "Answer 2-3" },
+    "Velho Testamento": [
+      { question: "Quem estava na fornalha de fogo ardente?",
+        answer: "Sadraque, Mesaque, Abedenego" },
+      { question: "Em qual cidade choveu fogo e enxofre?",
+        answer: "Sodoma e Gomorra" },
+      { question: "Qual o versículo mais longo da Bíblia?",
+        answer: "Ester 8:9 Então foram chamados os escrivães do rei, naquele mesmo tempo, no terceiro mês (que é o mês de Sivã), aos vinte e três dias; e se escreveu conforme a tudo quanto ordenou Mardoqueu aos judeus, como também aos sátrapas, e aos governadores, e aos líderes das províncias, que se estendem da Úndia até Etiópia, cento e vinte e sete províncias, a cada província segundo o seu modo de escrever, e a cada povo conforme a sua língua; como também aos judeus segundo o seu modo de escrever, e conforme a sua língua." },
     ],
-    "Category 3": [
-      { question: "Question 3-1", answer: "Answer 3-1" },
-      { question: "Question 3-2", answer: "Answer 3-2" },
-      { question: "Question 3-3", answer: "Answer 3-3" },
+    "Conhecimentos Gerais": [
+      { question: "Que dia da semana foi 2 de julho de 2001?",
+        answer: "Segunda" },
+      { question: "Qual a maior fronteira (em extensão) que a França tem com outro país?",
+        answer: "Brasil" },
+      { question: "Cite a maior quantidade de casa decimais de Pi",
+        answer: "3.141592653589793238462643383279" },
     ],
-    "Category 4": [
-      { question: "Question 4-1", answer: "Answer 4-1" },
-      { question: "Question 4-2", answer: "Answer 4-2" },
-      { question: "Question 4-3", answer: "Answer 4-3" },
+    "Bíblia ou Influencer": [
+      { question: "Não permita que a pressa roube a beleza do seu caminho. Tudo tem seu tempo e propósito",
+        answer: "INFLUENCER, Camila Coelho" },
+      { question: "Aquele que aguenta até o fim será salvo",
+        answer: "BÍBLIA! Mateus 24:13" },
+      { question: "A jornada é longa, mas a fé me sustenta. Eu sou maior que qualquer obstáculo",
+        answer: "INFLUENCER, Whindersson Nunes" },
     ],
-    "Category 5": [
-      { question: "Question 5-1", answer: "Answer 5-1" },
-      { question: "Question 5-2", answer: "Answer 5-2" },
-      { question: "Question 5-3", answer: "Answer 5-3" },
+    "Bíblia ou Influencer 2": [
+      { question: "O homem bom tira boas coisas do seu coração, e o homem mau tira coisas más",
+        answer: "BÍBLIA! Lucas 6:45" },
+      { question: "Os dias do homem são muito curtos, e não vale desperdiçá-los com aquilo que não traz a paz",
+        answer: "INFLUENCER, Bruna Marquezine" },
+      { question: "Se você não tem coragem para mudar, nunca será capaz de avançar",
+        answer: "INFLUENCER, Nathalia Arcuri" },
     ],
-    "Category 6": [
-      { question: "Question 6-1", answer: "Answer 6-1" },
-      { question: "Question 6-2", answer: "Answer 6-2" },
-      { question: "Question 6-3", answer: "Answer 6-3" },
+
+    "Bíblia ou Influencer 3": [
+      { question: "Quem ama, nunca desiste, mas suporta tudo com fé, esperança e paciência",
+        answer: "BÍBLIA, 1 Coríntios 13:7" },
+      { question: "Quem fala sem pensar, está pedindo para tomar uma surra",
+        answer: "BÍBLIA, Provérbios 18:6" },
+      { question: "O mundo te diz para seguir seus desejos, mas a sabedoria te ensina a seguir a verdade",
+        answer: "INFLUENCER, Monja Coen" },
     ],
   };
